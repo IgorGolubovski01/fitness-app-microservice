d21c322fea7a276429b393332aef029ce2e73a15
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2026-03-16 08:59:35 +0100

    Init

---
+++ userservice/userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -0,0 +1,55 @@
+package com.fitness.userservice.service;
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.model.User;
+import com.fitness.userservice.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository repository;
+
+    public @Nullable UserResponse register(@Valid RegisterRequest request) {
+
+        if(repository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email address already in use");
+        }
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+        repository.save(user);
+
+
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        return userResponse;
+
+    }
+
+    public @Nullable UserResponse getUserProfile(String userId) {
+        User user = repository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+
+        return userResponse;
+    }
+}
